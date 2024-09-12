fcf118a78d2d640c38539a829ba9a6b6a16e0370
Your Name <om.pawar1512@gamil.com>
2024-09-12 20:49:55 +0530

    made some utility or utils files for handleing errors and api response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-// require('dotenv').config({path : './env'})
-import dotenv from "dotenv"
-dotenv.config({path:'./env'})
+// require('dotenv').config({path : './env'}) // another way to configure .env
 
+import dotenv from "dotenv";
+dotenv.config({path:'./env'})
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
+
 connectDB()
+.then(()=>{
+
+    app.on("error" , (error)=>{
+        console.log("ERROR: " , error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 , () =>{ 
+        console.log(` server is running on port : ${process.env.PORT || 8000 }`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection Failed !!! " , err)
+})
 
 
 
